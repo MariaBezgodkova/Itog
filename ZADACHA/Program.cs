51a51d91f0ff2afd42df56f79cf32c41d4be0225
MariaBezgodkova <ask.2004@yandex.ru>
2023-03-26 13:53:15 +0300

    Решение задачи

---
+++ ZADACHA/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] t1 = new string[7] { "begin", "0", "text", "-$%", "%$#@*", "Home", "end" };
+string[] t2 = new string[t1.Length];
+void SearchThreeCharacters(string[] t1, string[] t2)
+{
+    int count = 0;
+    for (int i = 0; i < t1.Length; i++)
+    {
+        if (t1[i].Length <= 3) t2[count] = t1[i]; count++;
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SearchThreeCharacters(t1, t2);
+PrintArray(t2);
